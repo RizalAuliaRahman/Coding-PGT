94282e5c10f0ee854128e3685729ed1b89300dcb
RizalAuliaRahman <111293918+RizalAuliaRahman@users.noreply.github.com>
2022-08-17 12:54:21 +0700

    Update cari-java.bat

--- Coding-PGT/Kemampuan Dasar/Kemampuan Dasar 1/Kasus/cari-java.bat
+++ Coding-PGT/Kemampuan Dasar/Kemampuan Dasar 1/Kasus/cari-java.bat
@@ -4,5 +4,21 @@ javac Rizal.java
 
 java Rizal
 
-$ cari-java.sh $HOME/src
-Ada file Java pada direktori /home/bpdp/src/hari-01
+Hasil
+
+PS C:\> cd .\Coding-PGT\
+PS C:\Coding-PGT> cd '.\Kemampuan Dasar\'
+PS C:\Coding-PGT\Kemampuan Dasar> cd '.\Kemampuan Dasar 1\'
+PS C:\Coding-PGT\Kemampuan Dasar\Kemampuan Dasar 1> cd .\Kasus\
+PS C:\Coding-PGT\Kemampuan Dasar\Kemampuan Dasar 1\Kasus> dir
+
+
+    Directory: C:\Coding-PGT\Kemampuan Dasar\Kemampuan Dasar 1\Kasus
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+-a----         8/17/2022  12:43 PM             43 cari-java.bat
+
+
+PS C:\Coding-PGT\Kemampuan Dasar\Kemampuan Dasar 1\Kasus>
